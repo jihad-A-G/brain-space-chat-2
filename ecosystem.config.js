fbedfad9df5130351879dda38b44bee7907fcdc3
jihad-A-G <jihadabdlghani73@gmail.com>
2025-07-18 15:28:37 +0300

    Testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'development',
-        PORT: 5000
+        PORT: 8083
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 5000
+        PORT: 8083
       },
       // Auto restart settings
       autorestart: true,
