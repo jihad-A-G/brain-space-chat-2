51a8ccfd5c4d6a30854f3a058c396d05b6b0cfbb
jihad-A-G <jihadabdlghani73@gmail.com>
2025-07-30 11:22:12 +0300

    Added user name to offline online events

--- src/sockets/chat.ts
+++ src/sockets/chat.ts
@@ -518,6 +518,8 @@ export function chatSocket(io: Server) {
             onlineUsers.delete(userId);
             const lastSeen = new Date();
             const username = await User.findByPk(userId, { attributes: ['user_name'] });
+            console.log(`User ${username} (${userId}) went offline at ${lastSeen}`);
+
             // Update last_seen and status in DB
             await User.update(
               { last_seen: lastSeen, status: 'Offline' },
